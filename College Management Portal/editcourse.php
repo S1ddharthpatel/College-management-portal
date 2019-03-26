4eda8dcd3ec337c5c8d8ae4586dae00383d54bca
Siddharth Patel <sidsiddharthpatel@gmail.com>
2019-03-27 00:00:16 +0530

    Update editcourse.php

--- College Management Portal/editcourse.php
+++ College Management Portal/editcourse.php
@@ -11,19 +11,19 @@
 			//run query
 					$query="SELECT * from courses c JOIN faculty f JOIN department d on c.faculty_id=f.faculty_id AND c.department_id=d.department_id";
 					$course=mysqli_query($conn,$query);
-					$row=mysqli_fetch_array($course);
+					$row=mysqli_fetch_array($course1);
 			?>
 					<form action="updatecourse.php" method="post">
 					<input type="hidden" name="course_id" value="<?php echo $GET_['course_id'];?>"/> <br>
 					
 					Name of course: <input type="text" name="course_name" value="<?php echo $row['course_name'];?>"/> <br>			
 					Name of Faculty:<select name="faculty_id">
-								<?php foreach($course as $stu){?>
+								<?php foreach($course1 as $stu){?>
 								<option value="<?php echo $stu['faculty_id'];?>"><?php echo $stu['faculty_name'];?></option>
 								<?php }?>
 							 </select> <br>
 					Name of Department:<select name="department_id">
-									<?php foreach($course as $stu){?>
+									<?php foreach($course1 as $stu){?>
 									<option value="<?php echo $stu['department_id'];?>"><?php echo $stu['department_name'];?></option>
 									<?php }?>
 							  </select><br>
